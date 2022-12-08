57743e11fe31a7ee740f4ffc891759505a188eee
Black3is <107100761+Black3is@users.noreply.github.com>
2022-12-08 08:27:25 +0100

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Rng = new System.Windows.Forms.Button();
+            this.Number1 = new System.Windows.Forms.TextBox();
+            this.Number2 = new System.Windows.Forms.TextBox();
+            this.Number3 = new System.Windows.Forms.TextBox();
+            this.Number4 = new System.Windows.Forms.TextBox();
+            this.Number5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Rng
+            // 
+            this.Rng.Location = new System.Drawing.Point(282, 113);
+            this.Rng.Name = "Rng";
+            this.Rng.Size = new System.Drawing.Size(238, 48);
+            this.Rng.TabIndex = 0;
+            this.Rng.Text = "Random";
+            this.Rng.UseVisualStyleBackColor = true;
+            this.Rng.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Number1
+            // 
+            this.Number1.Location = new System.Drawing.Point(61, 349);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(100, 20);
+            this.Number1.TabIndex = 1;
+            this.Number1.TextChanged += new System.EventHandler(this.Number1_TextChanged);
+            // 
+            // Number2
+            // 
+            this.Number2.Location = new System.Drawing.Point(202, 349);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(100, 20);
+            this.Number2.TabIndex = 2;
+            this.Number2.TextChanged += new System.EventHandler(this.Number2_TextChanged);
+            // 
+            // Number3
+            // 
+            this.Number3.Location = new System.Drawing.Point(342, 349);
+            this.Number3.Name = "Number3";
+            this.Number3.Size = new System.Drawing.Size(100, 20);
+            this.Number3.TabIndex = 3;
+            this.Number3.TextChanged += new System.EventHandler(this.Number3_TextChanged);
+            // 
+            // Number4
+            // 
+            this.Number4.Location = new System.Drawing.Point(478, 349);
+            this.Number4.Name = "Number4";
+            this.Number4.Size = new System.Drawing.Size(100, 20);
+            this.Number4.TabIndex = 4;
+            this.Number4.TextChanged += new System.EventHandler(this.Number4_TextChanged);
+            // 
+            // Number5
+            // 
+            this.Number5.Location = new System.Drawing.Point(604, 349);
+            this.Number5.Name = "Number5";
+            this.Number5.Size = new System.Drawing.Size(100, 20);
+            this.Number5.TabIndex = 5;
+            this.Number5.TextChanged += new System.EventHandler(this.Number5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Number5);
+            this.Controls.Add(this.Number4);
+            this.Controls.Add(this.Number3);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.Rng);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Rng;
+        private System.Windows.Forms.TextBox Number1;
+        private System.Windows.Forms.TextBox Number2;
+        private System.Windows.Forms.TextBox Number3;
+        private System.Windows.Forms.TextBox Number4;
+        private System.Windows.Forms.TextBox Number5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,111 @@ namespace Lotto
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int[] RndArray = new int[5];
+            int[] RndArray2 = new int[5];
+
+            for (int i = 0; i < RndArray.Length; i++)
+            {   
+                RndArray[i] = rnd.Next(1,49);
+            }
+            for (int i = 0; i < RndArray2.Length; i++)
+            {
+                RndArray2[i] = rnd.Next(1, 49);
+            }
+            Number1.Text = RndArray[0].ToString();
+            Number2.Text = RndArray[1].ToString();
+            Number3.Text = RndArray[2].ToString();
+            Number4.Text = RndArray[3].ToString();
+            Number5.Text = RndArray[4].ToString();
+
+            if (RndArray[0] == RndArray2[0])
+            {
+                Number1.BackColor = Color.Green;
+            }
+
+            else
+            {
+                Number1.BackColor = Color.White;
+            }
+
+            if (RndArray[1] == RndArray2[1])
+            {
+                Number2.BackColor = Color.Green;
+            }
+
+            else
+            {
+                Number2.BackColor = Color.White;
+            }
+
+            if(RndArray[2] == RndArray2[2])
+            {
+                Number3.BackColor = Color.Green;
+            }
+
+            else
+            {
+                Number3.BackColor = Color.White;
+            }
+
+            if (RndArray[3] == RndArray2[3])
+            {
+                Number4.BackColor = Color.Green;
+            }
+
+            else
+            {
+                Number4.BackColor = Color.White;
+            }
+
+            if (RndArray[4] == RndArray2[4])
+            {
+                Number5.BackColor = Color.Green;
+            }
+
+            else
+            {
+                Number5.BackColor = Color.White;
+            }
+
+
+
+        }
+
+        private void Number1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Number2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Number3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Number4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Number5_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
